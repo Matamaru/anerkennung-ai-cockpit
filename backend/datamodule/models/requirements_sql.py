faa1269200006b23e1f862b46a3f87844700fb82
Matamaru <hmatamaru@outlook.de>
2025-11-29 03:46:20 +0100

    typo in req_sql corrected...

--- backend/datamodule/models/requirements_sql.py
+++ backend/datamodule/models/requirements_sql.py
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS _requirements (
     optional BOOLEAN DEFAULT FALSE,
     translation_required BOOLEAN DEFAULT FALSE,
     fullfilled BOOLEAN DEFAULT FALSE,
-    FOREIGN KEY (profession_id) REFERENCES _profession(id),
+    FOREIGN KEY (profession_id) REFERENCES _professions(id),
     FOREIGN KEY (country_id) REFERENCES _country(id),
     FOREIGN KEY (state_id) REFERENCES _states(id)
 );
