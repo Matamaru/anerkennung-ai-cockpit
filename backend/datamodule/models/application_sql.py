f12f1b65b7853908ecebb13999f879ed89f61579
Matamaru <hmatamaru@outlook.de>
2025-11-29 04:15:08 +0100

    another typo ...

--- backend/datamodule/models/application_sql.py
+++ backend/datamodule/models/application_sql.py
@@ -14,8 +14,8 @@ CREATE TABLE IF NOT EXISTS _applications (
     state_id VARCHAR(36) NOT NULL,
     time_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES _users(id),
-    FOREIGN KEY (profession_id) REFERENCES _profession(id),
-    FOREIGN KEY (country_id) REFERENCES _country(id),
+    FOREIGN KEY (profession_id) REFERENCES _professions(id),
+    FOREIGN KEY (country_id) REFERENCES _countries(id),
     FOREIGN KEY (state_id) REFERENCES _states(id)
 );
 """
